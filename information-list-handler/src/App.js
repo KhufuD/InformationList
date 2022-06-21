4cc05a0cc6dbb36354aa26ed7bd42b7fdef3dd09
khufu <khufudev@gmail.com>
2022-06-21 11:52:02 -0700

    List styled and fixed array in App.js

--- information-list-handler/src/App.js
+++ information-list-handler/src/App.js
@@ -1,21 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import Form from "./components/Form";
 import FormList from "./components/FormList";
 
 function App() {
-  const [userList,setUserList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
-  const addUserHandler = (uName,uAge) => {
-    setUserList(() => {
-      return [{name: uName, age: uAge}];
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
     });
-  }
+  };
   return (
     <div className="App">
-      <Form onAddUser = {addUserHandler}/>
-      <FormList users = {userList}/>
+      <Form onAddUser={addUserHandler} />
+      <FormList users={userList} />
     </div>
   );
 }
--- information-list-handler/src/components/FormList.js
+++ information-list-handler/src/components/FormList.js
@@ -7,7 +7,7 @@ const FormList = (props) => {
     <Card>
       <ul>
         {props.users.map((user) => (
-          <li>
+          <li key={user.id}>
             {user.name} ({user.age} years old)
           </li>
         ))}
